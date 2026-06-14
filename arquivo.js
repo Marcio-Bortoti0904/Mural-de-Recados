ad4a6b3831d27625ff7deb22bf61dc75017dbe92
Márcio Bortoti Júnior. <marcio.santo.junior@escola.pr.gov.br>
2026-06-14 16:23:34 -0300

    Atualizando arquivo.js...

--- arquivo.js
+++ arquivo.js
@@ -5,8 +5,6 @@ let Elemento = document.getElementById("Elemento");
 function adicionarRecado() {    
     if (Autor.value == "" || Recado.value == "") {
         alert("Preencha todos os campos.");
-        Autor.value = "";
-        Recado.value = "";
     } else {
         Elemento.innerHTML += "<li><strong>" + Autor.value + " diz:</strong> " + Recado.value + "</li>";
         Autor.value = "";
