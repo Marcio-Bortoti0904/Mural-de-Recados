e0b4d6b2a18f638a71f37b321e4ee887ff5fb3f4
Márcio Bortoti Júnior. <marcio.santo.junior@escola.pr.gov.br>
2026-06-17 16:32:32 -0300

    Refactor adicionarRecado function and add limpar_Resultado

--- arquivo.js
+++ arquivo.js
@@ -1,13 +1,15 @@
-let Autor = document.getElementById("Nome_do_autor");
-let Recado = document.getElementById("o_recado");
-let Elemento = document.getElementById("Elemento");
-
 function adicionarRecado() {    
-    if (Autor.value == "" || Recado.value == "") {
+    let Autor = document.getElementById("Nome_do_autor");
+    let Recado = document.getElementById("o_recado");
+    let Elemento = document.getElementById("Elemento");if (Autor.value == "" || Recado.value == "") {
         alert("Preencha todos os campos.");
     } else {
         Elemento.innerHTML += "<li><strong>" + Autor.value + " diz:</strong> " + Recado.value + "</li>";
         Autor.value = "";
         Recado.value = ""; 
     }
+}
+
+function limpar_Resultado() {
+    Elemento.innerHTML = "";
 }
